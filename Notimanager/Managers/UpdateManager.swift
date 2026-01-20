ff86090dbfe50718bf6790b37edfae2717e604d4
abd3lraouf <abdelraoufsabri@gmail.com>
2026-01-20 03:14:32 +0200

    chore: add dummy comment for testing 2.1.14 release

--- Notimanager/Managers/UpdateManager.swift
+++ Notimanager/Managers/UpdateManager.swift
@@ -210,3 +210,5 @@ extension UpdateManager: SPUUpdaterDelegate {
 // Dummy change for testing 2.1.14 release
 
 // Dummy change for testing 2.1.13 release
+
+// Dummy change for testing 2.1.14 release
